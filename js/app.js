d3838584099791d509b63cc262bad823e1fdfb50
Katie <kvidal01@wildcats.jwu.edu>
2019-09-29 23:25:20 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+
+/*@import 'header.scss';
+@import 'type.scss';
+@import 'vars';
+@import 'timeline-item';*/
+
+h2 {
+  font-weight: 700; 
+  @media screen and (min-width: 40em){
+    writing-mode: vertical-rl;
+    font-size: 10rem;
+    text-orientation: upright;
+    text-transform: uppercase;
+    letter-spacing: -25px;
+  }
+}
