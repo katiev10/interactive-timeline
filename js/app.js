b1f6ff73a8adb97acc2659e7c392e9fe98b82dea
Katie <kvidal01@wildcats.jwu.edu>
2019-10-11 10:20:27 -0400

    puck scrool

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 
-/*@import 'header.scss';
-@import 'type.scss';
-@import 'vars';
-@import 'timeline-item';*/
+// Parallax via https://www.kirupa.com/html5/smooth_parallax_scrolling.htm
 
-h2 {
-  font-weight: 700; 
-  @media screen and (min-width: 40em){
-    writing-mode: vertical-rl;
-    font-size: 10rem;
-    text-orientation: upright;
-    text-transform: uppercase;
-    letter-spacing: -25px;
-  }
+var bigYellowCircle = document.querySelector("#bigYellowCircle");
+var status = 0;
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+}
+ 
+window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+var xScrollPosition;
+var yScrollPosition;
+ 
+function scrollLoop() {
+  xScrollPosition = window.scrollX;
+  yScrollPosition = window.scrollY;
+
+  setTranslate(0, yScrollPosition * .01, bigYellowCircle);
+
+  
+  requestAnimationFrame(scrollLoop);
 }
 
